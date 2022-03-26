3c6215aaefdad97f8d304561b22ad94be802cb4c
Laura Smith <laurajayne8@live.com>
2022-03-26 14:50:43 +0000

    first commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,9 @@
+import React from "react"
+import { Link, Outlet } from "react-router-dom"
+
 function Nav() {
     return (
-        <div>Hello</div>
+        <Outlet />
     )
 }
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,24 @@
+import React from "react"
+import Logo from "../walkieslogo.png"
+
 function Login() {
     return (
-        <div>Hello</div>
+        <section classname = "fullpage">
+            <div>
+                <img src={Logo} alt = "Walkies Logo" />
+                <h1>Welcome to Walkies!</h1>
+                <h2>Login to your account here:</h2>
+                <section>
+                    <form>
+                        <label for="eMail">Email Address:</label><br></br>
+                        <input type="text" id="eMail" name="eMail"></input><br></br>
+                        <label for="password">Password:</label><br></br>
+                        <input type="text" id="password" name="password"></input><br></br>
+                        <input type="submit" value="Submit"></input>
+                    </form>
+                </section>
+            </div>
+        </section>
     )
 }
 
