d428e6d5132c6f5b47d871fb0f3267483b88ff2a
Claudia <claudiaescola7@gmail.com>
2022-03-29 19:26:39 +0100

    testing DB connection

--- src/components/dogs.js
+++ src/components/dogs.js
@@ -10,9 +10,10 @@ async function Dogs() {
 
             const dogData = await fetch(`http://localhost:4000/`)
             const dogMessage = await dogData.json()
+            const dogName = await dogMessage.name
             setDog(dogMessage)
             console.log(dogMessage)
-            console.log(dog)
+            console.log(dogName)
 
         }
         GetData()
