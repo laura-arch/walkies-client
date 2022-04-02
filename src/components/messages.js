0c9e2b200b79d0684cf998b30d0eaa85a2cc0a49
Claudia <claudiaescola7@gmail.com>
2022-04-02 01:40:34 +0100

    updated messages page

--- src/components/messages.js
+++ src/components/messages.js
@@ -3,11 +3,8 @@ import axios from 'axios'
 
 function Messages() {
     
-    const [messages, updateMessages] = React.useState('')
-    
+    const [messages, updateMessages] = React.useState('')   
     React.useEffect(() => {
-        console.log(localStorage)
-
         axios({
             method: 'get',
             url: 'https://walkies-backend.herokuapp.com/dogs',
@@ -16,28 +13,30 @@ function Messages() {
             }
         })
             .then(response => {
-
                 console.log(response.data);
                 updateMessages(response.data)
             })
             .catch(error => {
                 console.log(error);
             })
-    }, [])
-
+}, [])
 
 
     return (
-        <section>{
+        <section className="dogs-list"> 
+            <div >                    
+            <h1>Hello, Human! 🐶 🐾</h1><br /><h3>See what other users have been saying about your dogs.</h3>
+</div>
+          {
             messages ? messages.map(x => 
-                <div>
-                    <h2>{x.name}</h2>
+                <div className="dog-profile">
+           <br /> <h2>{x.name}</h2>
                     <img className="dog-image" alt="Dog" src={x.image}></img>
 
                     <h3>Messages:</h3>
                     <p>{x.comments}</p>
                 </div>) 
-            : <p>Loading your messages 🐾</p>}
+                    : <p>Loading your messages 🐾</p>}
             
         </section>
     )
