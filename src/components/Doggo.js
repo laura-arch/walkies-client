96e918745d46169f62db5563410bdfc72c87312d
Claudia <claudiaescola7@gmail.com>
2022-04-02 01:13:18 +0100

    updated register, dogs, individual dog page and messages. Incomplete though

--- src/components/Doggo.js
+++ src/components/Doggo.js
@@ -1,13 +1,40 @@
 import React from "react";
 import "../Styles/dog.css"
 import "../Styles/Forms.css"
+import axios from 'axios'
+import { Link, useParams } from "react-router-dom"
 
 function Doggo() {
+    const [dog, setDog] = React.useState(undefined);
+    const { dogId } = useParams() 
+
+    React.useEffect(() => {
+
+        console.log('this is dogId ', dogId)
+        axios({
+            method: 'get',
+            url: `https://walkies-backend.herokuapp.com/dogs/${dogId}`,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            }
+        })
+            .then(response => {
+                // Console logging the data
+                console.log(`doggo data: `);
+                console.log(response.data);
+
+                setDog(response.data);
+
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, [dogId])
 
     return (
         <>
-        <div class="content">
-            <img class="hero-image" src="https://i0.wp.com/ohmydogblog.com/wp-content/uploads/2010/03/Emmett.jpg?w=1200&ssl=1" alt="Dog"></img>
+        <div className="content">
+            <img className="hero-image" src="https://i0.wp.com/ohmydogblog.com/wp-content/uploads/2010/03/Emmett.jpg?w=1200&ssl=1" alt="Dog"></img>
             <div>
                 <h1>Emmett</h1>
                 <h2>Breed: Beagle</h2>
@@ -15,11 +42,21 @@ function Doggo() {
                 <h2>Location: South London</h2>
                 <h2>Availability: Weekends and evenings</h2>
                 <p>Oh, Emmett. Emmett is stubborn but so sweet, a little Velcro strip stuck to my pant-leg. He’s lazy and has serious food-hoarding issues. But he’ll do absolutely anything we ask of him, especially if there’s a treat involved. Emmett loves being the center of attention, loves meeting new people, loves going new places. Emmett is completely and utterly fearless. He tolerates what he doesn’t like – puppies, not being fed on time – with heavy sighs. But he’s happy, oh, is he happy. He wags his curly tail with his entire body. All he wants out of life is a good snuggle, a tasty snack, and a nice, long nap preferably outside, in the sun, on his back.</p>
-                <form action="http://localhost:3000/">
-                    <input type="submit" value="Mesage me!" />
-                </form>
             </div>
         </div>
+
+            {/* {dog ? dog.map(x => <div className="dog-profile">
+                <img className="dog-image" alt="Dog" src={x.image}></img>
+                <h1>{x.name}</h1>
+                <h2>Breed: {x.breed}</h2>
+                <h2>Age: {x.age}</h2>
+                <h2>Location: {x.location}</h2>
+                <h2>About this Doggo: {x.description}</h2>
+                <h2>Availability: {x.availability}</h2>
+            </div>) : <p>Loading</p>}
+            
+            use this for messages endpoint ${x._id} */}
+            <Link to={`/messages/`}><button>Message me 🐾</button></Link>
         </>
     )
 }
--- src/components/register.js
+++ src/components/register.js
@@ -5,89 +5,32 @@ import axios from 'axios'
 
 function Register() {
 
-    // States for registration
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [profilePic, setProfilePic] = React.useState('')
-    const [location, setLocation] = React.useState('')
-    const [aboutMe, setAboutMe] = React.useState('')
-    const [availability, setAvailability] = React.useState('')
-    const [role, setRole] = React.useState('')
-    const [user, updateUser] = React.useState('')
-
     // States for checking the errors
     const [submitted, setSubmitted] = React.useState(false);
     const [error, setError] = React.useState(false);
- 
+    const [formData, setFormData] = React.useState({})
 
-    //POST user
-    React.useEffect(() => {
-
-        async function fetchData() {
-            const user = { name: name, eMail: email, password: password, image: profilePic, location: location, aboutMe: aboutMe, availability: availability, role: role };
-            const resp = await axios.post('https://walkies-backend.herokuapp.com/register', user)
-            updateUser(resp)  
-            console.log(resp)
-        }
-
-        fetchData();
-    }, )
-    console.log(user)
-
-    // Handling the name change
-    const handleName = (e) => {
-        setName(e.target.value);
+    // Handling onChange
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
         setSubmitted(false);
-    };
-
-    // Handling the email change
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-        setSubmitted(false);
-    };
-
-    // Handling the password change
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-        setSubmitted(false);
-    };
-
-    const handleProfilePic = (e) => {
-        setProfilePic(e.target.value);
-        setSubmitted(false);
-    };
-
-    const handleLocation = (e) => {
-        setLocation(e.target.value);
-        setSubmitted(false);
-    };
-
-    const handleAboutMe = (e) => {
-        setAboutMe(e.target.value);
-        setSubmitted(false);
-    };
-
-    const handleAvailability = (e) => {
-        setAvailability(e.target.value);
-        setSubmitted(false);
-    };
-
-    const handleRole = (e) => {
-        setRole(e.target.value);
-        setSubmitted(false);
-    };
+    }
 
     // Handling the form submission
-    const handleSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
-        if (name === '' || email === '' || password === '' || profilePic === '' || location === '' || aboutMe === '' || availability === '' || role === '') {
-            setError(true);
-        } else {
+
+        try {
+            const resp = await axios.post('https://walkies-backend.herokuapp.com/register', formData)
+            console.log(resp)
             setSubmitted(true);
             setError(false);
         }
+        catch (e) {
+            setError(e.response.data.message)
+        }
     };
+
 
     // Showing success message
     const successMessage = () => {
@@ -97,7 +40,7 @@ function Register() {
                 style={{
                     display: submitted ? '' : 'none',
                 }}>
-                <h1>User {name} successfully registered! 🐶🐾 </h1>
+                <h1>User successfully registered! 🐶🐾 </h1>
             </div>
         );
     };
@@ -121,50 +64,50 @@ function Register() {
                 <h1>User Registration 🐶🐾</h1>
             </div>
             {/* Calling to the methods */}
+          
+
+            <form onSubmit={onSubmit}>
+                {/* Labels and inputs for form data */}
+                <label className="label">Name</label>
+                <input onChange={onChange} className="input"
+                    name='name' type="text" />
+
+                <label className="label">Email</label>
+                <input onChange={onChange} className="input"
+                    name='email' type="email" />
+
+                <label className="label">Password</label>
+                <input onChange={onChange} className="input"
+                    name='password' type="password" />
+
+                <label className="label">Profile picture (URL)</label>
+                <input onChange={onChange} className="input"
+                    name='profilePic' type="text" />
+
+                <label className="label">Location</label>
+                <input onChange={onChange} className="input"
+                    name= 'location' type="text" />
+
+                <label className="label">About Me</label>
+                <input onChange={onChange} className="input"
+                    name='aboutMe' type="text" />
+
+                <label className="label">Availability (Monday-Sunday)</label>
+                <input onChange={onChange} className="input"
+                    name='availability' type="text" />
+
+                <label className="label">Are you a Owner or a Borrower 🐶🐾 </label>
+                <input onChange={onChange} className="input"
+                    name='role' type="text"/>
+
+                <button className="button" type="submit">
+                    Submit
+                </button>
+            </form>
             <div className="messages">
                 {errorMessage()}
                 {successMessage()}
             </div>
-
-            <form>
-                {/* Labels and inputs for form data */}
-                <label className="label">Name</label>
-                <input onChange={handleName} className="input"
-                    value={name} type="text" />
-
-                <label className="label">Email</label>
-                <input onChange={handleEmail} className="input"
-                    value={email} type="email" />
-
-                <label className="label">Password</label>
-                <input onChange={handlePassword} className="input"
-                    value={password} type="password" />
-
-                <label className="label">Profile picture (URL)</label>
-                <input onChange={handleProfilePic} className="input"
-                    value={profilePic} type="text" />
-
-                <label className="label">Location</label>
-                <input onChange={handleLocation} className="input"
-                    value={location} type="text" />
-
-                <label className="label">About Me</label>
-                <input onChange={handleAboutMe} className="input"
-                    value={aboutMe} type="text" />
-
-                <label className="label">Availability (Monday-Sunday)</label>
-                <input onChange={handleAvailability} className="input"
-                    value={availability} type="text" />
-
-                <label className="label">Are you a Owner or a Borrower 🐶🐾 </label>
-                <input onChange={handleRole} className="input"
-                    value={role} type="text"/>
-
-                <button className="button" onClick={handleSubmit} type="submit">
-                    Submit
-                </button>
-            </form>
-           
         </div>
     );
 }
